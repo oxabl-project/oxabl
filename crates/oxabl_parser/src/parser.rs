a275c87339c1c16c94b42dd6fa5ea4e9e6555eec
Evan Robertson <evan.robertson77@gmail.com>
2026-01-25 18:37:44 -0500

    feat(parser): Can parse longer expressions

--- crates/oxabl_parser/src/parser.rs
+++ crates/oxabl_parser/src/parser.rs
@@ -2,10 +2,19 @@
 //!
 //! Will panic if peek or advance are called when current cursor position
 //! is out of bounds. Contract is to check is_end before using them.
-use oxabl_ast::Expression;
+
+use oxabl_ast::{Expression, Span};
 use oxabl_lexer::{Kind, Token};
 
 use crate::literal::token_to_literal;
+
+#[derive(Debug)]
+pub struct ParseError {
+    pub message: String,
+    pub span: Span,
+}
+
+pub type ParseResult<T> = Result<T, ParseError>;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Parser<'a> {
@@ -38,30 +47,39 @@ impl<'a> Parser<'a> {
         self.check(Kind::Eof)
     }
 
-    pub fn parse_expression(&mut self) -> Expression {
+    pub fn parse_expression(&mut self) -> ParseResult<Expression> {
         // parse 1 + 1
         // TODO - operator precedence
-        let left_exp = self.parse_primary().expect("Expected an expression");
+        let mut expr = self.parse_primary()?;
         // check for "+" operand
-        if self.check(Kind::Add) {
-            // TODO - change to a while to keep consuming operators
+        while self.check(Kind::Add) {
             let _operator = self.advance();
-            let right_exp = self.parse_primary().expect("Expected an expression");
-            return Expression::Add(Box::new(left_exp), Box::new(right_exp));
-        } else {
-            left_exp
+            let right_exp = self.parse_primary()?;
+            expr = Expression::Add(Box::new(expr), Box::new(right_exp));
         }
+        Ok(expr)
     }
 
-    pub fn parse_primary(&mut self) -> Option<Expression> {
+    pub fn parse_primary(&mut self) -> ParseResult<Expression> {
         if self.check(Kind::IntegerLiteral) {
             let token = self.advance();
-            let literal = token_to_literal(token).expect("Thought I'd get a literal bro");
+            let literal = token_to_literal(token).ok_or_else(|| ParseError {
+                message: format!("Failed to convert token to literal"),
+                span: Span {
+                    start: token.start as u32,
+                    end: token.end as u32,
+                },
+            })?;
             let expression = Expression::Literal(literal);
-            return Some(expression);
-        } else {
-            None
+            return Ok(expression);
         }
+        Err(ParseError {
+            message: format!("Unexpected token {:?}", self.peek().kind),
+            span: Span {
+                start: self.peek().start as u32,
+                end: self.peek().end as u32,
+            },
+        })
     }
 }
 
@@ -72,10 +90,11 @@ mod test {
     use oxabl_lexer::tokenize;
 
     #[test]
-    fn parse_add_expression() {
-        let tokens = tokenize("1 + 1");
+    fn parse_simple_add_expression() {
+        let tokens = tokenize("1 + 2");
         let mut parser = Parser::new(&tokens);
-        let expression = parser.parse_expression();
+        let expression = parser.parse_expression().expect("Expected an expression");
+        println!("{:?}", expression);
         assert_eq!(
             expression,
             Expression::Add(
@@ -85,7 +104,34 @@ mod test {
                 }))),
                 Box::new(Expression::Literal(Literal::Integer(IntegerLiteral {
                     span: Span { start: 4, end: 5 },
-                    value: 1
+                    value: 2
+                })))
+            )
+        );
+    }
+
+    #[test]
+    fn parse_double_add_expression() {
+        let tokens = tokenize("1 + 2 + 3");
+        let mut parser = Parser::new(&tokens);
+        let expression = parser.parse_expression().expect("Expected an expression");
+        println!("{:?}", expression);
+        assert_eq!(
+            expression,
+            Expression::Add(
+                Box::new(Expression::Add(
+                    Box::new(Expression::Literal(Literal::Integer(IntegerLiteral {
+                        span: Span { start: 0, end: 1 },
+                        value: 1
+                    }))),
+                    Box::new(Expression::Literal(Literal::Integer(IntegerLiteral {
+                        span: Span { start: 4, end: 5 },
+                        value: 2
+                    })))
+                )),
+                Box::new(Expression::Literal(Literal::Integer(IntegerLiteral {
+                    span: Span { start: 8, end: 9 },
+                    value: 3
                 })))
             )
         );
