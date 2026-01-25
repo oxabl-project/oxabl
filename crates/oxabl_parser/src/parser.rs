94d9deec9a5e7970ba509059e6e796d483909f0f
Evan Robertson <evan.robertson77@gmail.com>
2026-01-25 18:15:17 -0500

    fix(parser): Fix test with missing import

--- crates/oxabl_parser/src/parser.rs
+++ crates/oxabl_parser/src/parser.rs
@@ -43,7 +43,8 @@ impl<'a> Parser<'a> {
         // TODO - operator precedence
         let left_exp = self.parse_primary().expect("Expected an expression");
         // check for "+" operand
-        if self.check(Kind::Add) { // TODO - change to a while to keep consuming operators
+        if self.check(Kind::Add) {
+            // TODO - change to a while to keep consuming operators
             let _operator = self.advance();
             let right_exp = self.parse_primary().expect("Expected an expression");
             return Expression::Add(Box::new(left_exp), Box::new(right_exp));
@@ -62,35 +63,30 @@ impl<'a> Parser<'a> {
             None
         }
     }
-
 }
 
 #[cfg(test)]
 mod test {
-    use oxabl_ast::{IntegerLiteral, Span};
-    use oxabl_lexer::tokenize;
     use super::*;
+    use oxabl_ast::{IntegerLiteral, Literal, Span};
+    use oxabl_lexer::tokenize;
 
     #[test]
-    fn parse_add_expression(){
+    fn parse_add_expression() {
         let tokens = tokenize("1 + 1");
         let mut parser = Parser::new(&tokens);
         let expression = parser.parse_expression();
         assert_eq!(
             expression,
             Expression::Add(
-                Box::new(Expression::Literal(
-                    Literal::Integer(IntegerLiteral {
-                        span: Span { start: 0, end: 1 },
-                        value: 1
-                    })
-                )),
-                Box::new(Expression::Literal(
-                    Literal::Integer(IntegerLiteral {
-                        span: Span { start: 4, end: 5 },
-                        value: 1
-                    })
-                ))
+                Box::new(Expression::Literal(Literal::Integer(IntegerLiteral {
+                    span: Span { start: 0, end: 1 },
+                    value: 1
+                }))),
+                Box::new(Expression::Literal(Literal::Integer(IntegerLiteral {
+                    span: Span { start: 4, end: 5 },
+                    value: 1
+                })))
             )
         );
     }
